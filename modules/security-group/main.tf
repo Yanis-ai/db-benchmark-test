43af8f98f638cbaabe10e06bbbfde803864b33ba
Yanis <yanh@sanoryza.com>
2025-02-16 07:36:35 +0800

    0216

---
+++ modules/security-group/main.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "main" {
+  name        = "db-benchmark-test-sg"
+  vpc_id      = var.vpc_id
+  description = "Security group for db-benchmark-test EC2 instances"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access from anywhere"
+  }
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allow HTTP access from anywhere"
+#   }
+
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allow HTTPS access from anywhere"
+#   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "db-benchmark-test-sg"
+  }
+}
+
+resource "aws_security_group_rule" "ssh_access" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+  depends_on = [aws_security_group.main]
+  description = "Allow SSH access from anywhere using key pair"
+  source_security_group_id = aws_security_group.main.id
+}
